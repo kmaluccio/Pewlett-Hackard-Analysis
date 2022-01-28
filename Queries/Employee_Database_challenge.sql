ab808a015b7f3784be84d66192e46c574b9bc956
kmaluccio <kmaluccio@tamu.edu>
2022-01-27 23:32:14 -0600

    add sql code for creating csv files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -20,3 +20,17 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 -- Check table
 SELECT * FROM retiring_titles;
+
+-- Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+-- Check the table looks correct before exporting to folder
+SELECT * FROM mentorship_eligibility;
